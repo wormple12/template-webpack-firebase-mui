d7823886e47816369e02065450d15cfa44323a98
wormple12 <wormple12@hotmail.com>
2021-08-31 13:16:03 +0200

    Firebase.config.ts added to template. Firebase init not yet done.

---
+++ src/services/firebase/firebase.config.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getAnalytics } from "firebase/analytics";
+// See SDKs for Firebase products that you want to use:
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDATp-oc4awN1bgsyTYlgI3_P2_hQUU_CY",
+    authDomain: "wormple-test.firebaseapp.com",
+    projectId: "wormple-test",
+    storageBucket: "wormple-test.appspot.com",
+    messagingSenderId: "708365944526",
+    appId: "1:708365944526:web:c056a2de4104eb1a6bf29f",
+    measurementId: "G-TR4L68EZGD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
