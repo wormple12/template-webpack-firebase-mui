dadaba9d3df955d8e8dbd2ab9014cb5b5a848198
wormple12 <wormple12@hotmail.com>
2021-09-09 13:12:05 +0200

    comment added about Jest/Puppeteer

--- src/services/swapi/swFetcher.test.ts
+++ src/services/swapi/swFetcher.test.ts
@@ -1,6 +1,18 @@
 import swFetcher from "./swFetcher";
 import { pilotOptions } from '@Types/SWSearch';
 
+/* 
+Jest doesn't properly support ECMAScript Modules import/export yet (I tried
+following their directions, but couldn't make it work), so I will either have to
+wait or find another testing framework.
+
+So far the plan was to implement Puppeteer with Jest, as seen in the following
+Academind video:
+https://www.youtube.com/watch?v=r9HdJ8P6GQI&ab_channel=Academind 
+
+But I don't know if Puppeteer works without Jest. More research needed.
+*/
+
 test("swFetcher should be able to fetch all pilots", async () => {
     await Promise.all(pilotOptions.map(p => {
         const pilot = swFetcher.getPilot(p).read();
